87149b1a385db32f585b56204258314b4e314efa
shichen.liu <shichen.liu@egreatwall.com>
2023-04-03 16:20:40 +0800

    feat: 登录页面

--- src/views/login.vue
+++ src/views/login.vue
@@ -16,11 +16,11 @@
           <div class="cf-flex-col section_3">
             <span class="cf-self-center text_3">Log in to AIbrush</span>
             <div class="cf-flex-col group_3">
-              <div class="space-x-10 cf-flex-row cf-items-end group_4">
+              <div class="space-x-10 cf-flex-row cf-items-end group_4 flex-cloum">
                 <span class="font_3 text_4">Don't have an account?</span>
-                <router-link :to="{ name: 'register' }"
-                  ><span class="font_2 signup">Sign up for free</span></router-link
-                >
+                <router-link :to="{ name: 'register' }"></router-link>
+                  <span class="font_2 signup">Sign up for free</span>
+                </router-link>
               </div>
             </div>
             <div class="cf-flex-col group_5 space-y-42">
@@ -31,8 +31,9 @@
                   class="cf-shrink-0 image_6"
                   src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/64053c035a7e3f0310f40af8/640540e0b98f5d0011592160/17c6af47efa2642231aa47aa4d4fe0a5.png"
                 />
-                <a :href="googleAuth"><span class="font_4">Continue with Goolge</span></a>
-                
+                <a :href="googleAuth">
+                  <span class="font_4">Continue with Goolge</span>
+                </a>
               </div>
               <div class="space-x-12 cf-flex-row cf-items-center">
                 <div class="section_5 view"></div>
@@ -45,7 +46,12 @@
                   <!-- <div class="cf-flex-col cf-justify-start cf-items-start cf-self-center text-wrapper_3">
                   <span class="font_2 text_6">cofocola@hotmail.com</span>
                 </div> -->
-                  <input class="text-wrapper_3" type="text" placeholder="email" v-model="email" />
+                  <input
+                    class="text-wrapper_3"
+                    type="text"
+                    placeholder="email"
+                    v-model="email"
+                  />
                 </div>
                 <div class="cf-flex-col group_6 space-y-11">
                   <span class="cf-self-start font_3 text_7">Password</span>
@@ -65,9 +71,12 @@
                 >
                   <span class="font_4">Continue with email</span>
                 </div>
-                <router-link to="/forgetPassword" class="cf-self-center font_3 text_8"
-                  >Forgot password?</router-link
+                <router-link
+                  to="/forgetPassword"
+                  class="cf-self-center font_3 text_8"
                 >
+                  Forgot password?
+                </router-link>
                 <div class="cf-self-center section_6"></div>
               </div>
             </div>
@@ -77,7 +86,7 @@
             v-show="errorMsg"
           >
             <span class="font_2">
-              {{errorMsg}}
+              {{ errorMsg }}
             </span>
           </div>
         </div>
@@ -86,59 +95,65 @@
   </div>
 </template>
 <script>
-import queryString from 'query-string';
-import trackEvent from "@/utils/trackEvent";
+import queryString from 'query-string'
+import trackEvent from '@/utils/trackEvent'
 
 export default {
-  name: "login",
+  name: 'login',
   data() {
     return {
-      email: "",
-      password: "",
-      emailErrorMessage: "",
-      passwordErrorMessage: "",
-      errorMsg: "",
-      gAccessToken: "",
-      googleAuth: "",
-    };
+      email: '',
+      password: '',
+      emailErrorMessage: '',
+      passwordErrorMessage: '',
+      errorMsg: '',
+      gAccessToken: '',
+      googleAuth: '',
+    }
   },
   computed: {
     emailError() {
       if (this.$refs.email) {
-        return !this.$refs.email.checkValidity();
+        return !this.$refs.email.checkValidity()
       }
-      return false;
+      return false
     },
     passwordError() {
       if (this.$refs.password) {
-        return !this.$refs.password.checkValidity();
+        return !this.$refs.password.checkValidity()
       }
-      return false;
+      return false
     },
     ch() {
-      return this.$route.query.ch || sessionStorage.getItem("ch") || "default";
+      return this.$route.query.ch || sessionStorage.getItem('ch') || 'default'
     },
     subchannel() {
-      return this.$route.query.subchannel || sessionStorage.getItem("subchannel") || "default";
+      return (
+        this.$route.query.subchannel ||
+        sessionStorage.getItem('subchannel') ||
+        'default'
+      )
     },
   },
   methods: {
     async submit() {
       if (!this.email) {
-        this.errorMsg = "Please enter your email";
-        return;
+        this.errorMsg = 'Please enter your email'
+        return
       }
-      if (!this.email.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/)) {
-        this.errorMsg = "Please enter a valid email address";
-        return;
+      if (
+        !this.email.match(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/)
+      ) {
+        this.errorMsg = 'Please enter a valid email address'
+        return
       }
       if (!this.password) {
-        this.errorMsg = "Please enter your password";
-        return;
+        this.errorMsg = 'Please enter your password'
+        return
       }
       if (this.password.length < 6) {
-        this.errorMsg = "Password must be at least 6 characters";
-        return;
+        this.errorMsg = 'Password must be at least 6 characters'
+        return
       }
       const data = {
         loginEmail: this.email,
@@ -146,80 +161,80 @@ export default {
         ch: this.ch,
         subchannel: this.subchannel,
         //1 注册 2 登录
-        loginType: "2",
-      };
-      let formData = new FormData();
+        loginType: '2',
+      }
+      let formData = new FormData()
       for (let key in data) {
-        formData.append(key, data[key]);
+        formData.append(key, data[key])
       }
       try {
-
         const response = await this.$axios.post(
-          "/api/WebLogin/interface",
-          formData
-        );
-        console.log({ response });
+          '/api/WebLogin/interface',
+          formData,
+        )
+        console.log({ response })
         if (response.result === '0') {
-          const {token, paySwapNum, userId: ui} = response.data;
+          const { token, paySwapNum, userId: ui } = response.data
           localStorage.setItem('token', token)
           localStorage.setItem('ui', ui)
           localStorage.setItem('paySwapNum', paySwapNum)
-          this.$router.push({ path: "/loginSuc" });
+          this.$router.push({ path: '/loginSuc' })
         } else {
-          this.errorMsg = response.message;
+          this.errorMsg = response.message
         }
-
-
       } catch (e) {
-        console.log({ e });
+        console.log({ e })
       }
     },
     async getUserInfo(ui) {
       try {
-        const formData = new FormData();
-        formData.append("ui", ui);
+        const formData = new FormData()
+        formData.append('ui', ui)
         //todo 后端没有返回userName， haedImg
-        const response = await this.$axios.post("/api/GetWebUserInfo/interface", formData);
-        console.log({ response });
+        const response = await this.$axios.post(
+          '/api/GetWebUserInfo/interface',
+          formData,
+        )
+        console.log({ response })
       } catch (e) {
-        console.log({ e });
+        console.log({ e })
       }
     },
     async googleLogin() {
       try {
-        if (!this.gAccessToken) return;
-        const formData = new FormData();
-        formData.append("accessToken", this.gAccessToken);
-        formData.append("ch", this.ch);
-        formData.append("subchannel", this.subchannel);
+        if (!this.gAccessToken) return
+        const formData = new FormData()
+        formData.append('accessToken', this.gAccessToken)
+        formData.append('ch', this.ch)
+        formData.append('subchannel', this.subchannel)
         const response = await this.$axios.post(
-          "/api/GoogleLogin/interface",
-          formData
-        );
+          '/api/GoogleLogin/interface',
+          formData,
+        )
         if (response.result === '0') {
-          const {token, paySwapNum, userId: ui} = response.data;
+          const { token, paySwapNum, userId: ui } = response.data
           localStorage.setItem('token', token)
           localStorage.setItem('ui', ui)
           localStorage.setItem('paySwapNum', paySwapNum)
           // await this.getUserInfo(ui);
-          this.$router.push({ path: "/" });
+          this.$router.push({ path: '/' })
         }
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
     },
   },
   async mounted() {
-    trackEvent("login", "pageShow", {ui: localStorage.getItem('ui')});
-    const hash = window.location.hash.substring(1);
-    const hashObject = queryString.parse(hash);
-    console.log('hashObject', hashObject);
-    this.gAccessToken = hashObject.access_token;
+    trackEvent('login', 'pageShow', { ui: localStorage.getItem('ui') })
+    const hash = window.location.hash.substring(1)
+    const hashObject = queryString.parse(hash)
+    console.log('hashObject', hashObject)
+    this.gAccessToken = hashObject.access_token
     if (this.gAccessToken) {
-      this.googleLogin();
+      this.googleLogin()
     }
   },
-};
+}
 </script>
 <style lang="scss" scoped>
 .signup {
@@ -240,7 +255,7 @@ export default {
     overflow-x: hidden;
     .section {
       padding: 0.3125vw 8.3333vw;
-      background-image: url("https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/64053c035a7e3f0310f40af8/640540e0b98f5d0011592160/16780660760883145281.png");
+      background-image: url('https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/64053c035a7e3f0310f40af8/640540e0b98f5d0011592160/16780660760883145281.png');
       background-position: 0% 0%;
       background-size: 100% 100%;
       background-repeat: no-repeat;
@@ -256,7 +271,7 @@ export default {
         .text {
           color: #ffffff;
           font-size: 1.4583vw;
-          font-family: "PingFang SC";
+          font-family: 'PingFang SC';
           font-weight: 500;
           line-height: 2.0833vw;
         }
@@ -267,7 +282,7 @@ export default {
         }
         .font_1 {
           font-size: 1.0417vw;
-          font-family: "PingFang SC";
+          font-family: 'PingFang SC';
           line-height: 1.4583vw;
           font-weight: 500;
           color: #ffffff;
@@ -290,7 +305,7 @@ export default {
           .text_2 {
             color: #ffffff;
             font-size: 0.9375vw;
-            font-family: "PingFang SC";
+            font-family: 'PingFang SC';
             font-weight: 500;
             line-height: 1.3021vw;
           }
@@ -340,7 +355,7 @@ export default {
           .text_3 {
             color: #ffffff;
             font-size: 2.0833vw;
-            font-family: "PingFang SC";
+            font-family: 'PingFang SC';
             font-weight: 700;
             line-height: 2.9167vw;
           }
@@ -433,7 +448,7 @@ export default {
             }
             .font_4 {
               font-size: 1.0417vw;
-              font-family: "PingFang SC";
+              font-family: 'PingFang SC';
               line-height: 1.4583vw;
               font-weight: 700;
               color: #ffffff;
@@ -455,7 +470,7 @@ export default {
           }
           .font_3 {
             font-size: 1.0417vw;
-            font-family: "PingFang SC";
+            font-family: 'PingFang SC';
             line-height: 1.4583vw;
             color: #ffffff66;
           }
@@ -472,7 +487,7 @@ export default {
         }
         .font_2 {
           font-size: 1.0417vw;
-          font-family: "PingFang SC";
+          font-family: 'PingFang SC';
           line-height: 1.4583vw;
           color: #ffffff;
         }
@@ -484,5 +499,73 @@ export default {
       right: 0;
     }
   }
+}
+@media screen and (max-width: 750px) {
+  .page .group .section_2 .pos_3 {
+    display: flex;
+    justify-content: center;
+  }
+  .page .group .section_2 .pos_3 .section_3{
+    width: 81.6667vw;
+    padding: 24.6875vw 0 7.9687vw;
+  }
+  .page .group .section_2 .pos_3 .section_3 .text_3{
+    font-size: 14px;
+    line-height: 20px;
+  }
+  .page .group .section_2 .pos_3 .section_3 .group_5 .text-wrapper_3{
+    padding: 3.7813vw;
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 0.5208vw;
+    width: 60.6875vw;
+    margin-left: 10.9896vw
+  }
+  .page .group{
+    padding-bottom: 168.8021vw;
+    overflow-x: hidden;
+  }
+  .page .group .section_2 .pos_3 .section_3 .group_5 .button_2 {
+    margin-top: 2.0833vw;
+    padding: 3.7813vw 0;
+    background-color: #7116db;
+    border-radius: 1.5104vw;
+    width: 60.6875vw;
+    cursor: pointer;
+}
+.page .group .section_2 .pos_3 .section_3 .group_5 .font_4{
+  font-size: 14px;
+}
+.page .group .section_2 .pos_3 .section_3 .group_5 .text_8 {
+    margin-top: 11.0938vw;
+}
+
+.page .group .section_2 .pos_3 .section_3 .font_3 {
+    font-size: 14px;
+    font-family: "PingFang SC";
+    line-height: 20px;
+    color: rgba(255, 255, 255, 0.4);
+}
+.page .group .section_2 .pos_3 .section_3 .group_3{
+  margin-top: 35.1979vw;
+}
+.page .group .section_2 .pos_3 .section_3 .group_5 .space-x-12 .section_5{
+  width: 27.6979vw;
+}
+.flex-cloum{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.page .group .section_2 .pos_3 .section_3 .group_3 .group_4 .text_4{
+  margin: 0;
+}
+.page .group .group_2 .image_4{
+    width: 50vw;
+    height: 158.8542vw;
+}
+.page .group .group_2 .image_5 {
+    width: 41.6667vw;
+    height: 158.8542vw;
+}
 }
 </style>
